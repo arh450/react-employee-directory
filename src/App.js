f5cb4869d1c955ed90ede98e563d4b873facaae4
hank amadeus <hankamadeus@email.com>
2020-08-06 11:57:45 -0400

    added nav header and json data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "./App.css";
 
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
     </div>
   );
 }
 
 export default App;
+
+// User should be able to sort table by at least one category
+// and filter users by at least one property
+
+// Components
+// navbar
+// Filter Object
+// Table
+// Properties (table headers)
+// Table data (user records)
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <nav class="navbar navbar-dark bg-dark p-4">
+      <h1 class="text-light mx-auto ">Employee Directory</h1>
+    </nav>
+  );
+}
