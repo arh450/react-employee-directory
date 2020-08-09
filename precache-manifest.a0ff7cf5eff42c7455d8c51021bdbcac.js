c9379484036b9e7220536827c78d0138cb55aecb
hank amadeus <hankamadeus@email.com>
2020-08-09 10:10:17 -0400

    Updates

---
+++ precache-manifest.a0ff7cf5eff42c7455d8c51021bdbcac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0682c37d658141e616502e90739f9fc2",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "adf2aa33c9cba5d8179b",
+    "url": "/react-employee-directory/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "6aa267fb547a99692df1",
+    "url": "/react-employee-directory/static/css/main.8b445657.chunk.css"
+  },
+  {
+    "revision": "adf2aa33c9cba5d8179b",
+    "url": "/react-employee-directory/static/js/2.207a33c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.207a33c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aa267fb547a99692df1",
+    "url": "/react-employee-directory/static/js/main.a269c67e.chunk.js"
+  },
+  {
+    "revision": "193cdcbd01f85fb428f8",
+    "url": "/react-employee-directory/static/js/runtime-main.76415422.js"
+  }
+]);
